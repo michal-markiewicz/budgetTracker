ff5ede434b0905cfc4abad3784f63b484c57003c
Michał Markiewicz <michalmarkiewicz0953@gmail.com>
2022-03-06 07:11:28 +0100

    Project directory

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+    1. Be able to record expenses and income
+    2. Be able to set the date for an entry
+    3. Be able to tag the entries to categorize them
+    4. Be able to see a monthly overview of expenses and income
+    5. Be able to see net worth
+    6. Be able to visualize the expenses and incomes through graphs
+    7. Be able to select date ranges, eg.: previous month, or year to date
+    8. Use localStorage to save all the data
+*/
+
