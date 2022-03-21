72b4f1702a43a87286f6d8df06a2787683e23a0d
Michał Markiewicz <michalmarkiewicz0953@gmail.com>
2022-03-21 16:05:18 +0100

    filterTableByDate, some refactoring

--- js/main.js
+++ js/main.js
@@ -11,10 +11,16 @@
     8. Use localStorage to save all the data
 */
 
-reloadHtmlTable();
+const tableBodyElement = document.querySelector('.financial-table-body');
 
 const incomeInputContainer = document.querySelector("#income-input-container");
 const expenseInputContainer = document.querySelector("#expense-input-container");
+
+const totalIncomeHeader = document.querySelector('#total-income');
+const totalExpenseHeader = document.querySelector('#total-expense');
+const totalBalanceHeader = document.querySelector('#total-balance');
+
+reloadHtmlTable();
 
 incomeInputContainer.querySelector(".submit-button").addEventListener('click', () => {
 
@@ -38,7 +44,7 @@ expenseInputContainer.querySelector(".submit-button").addEventListener('click', 
     const expenseName = expenseInputContainer.querySelector('#expense-name');
     const expenseAmount = expenseInputContainer.querySelector('#expense-amount');
     const expenseDate = expenseInputContainer.querySelector('#expense-date');
-    const expenseCategory = incomeInputContainer.querySelector('#expense-category');
+    const expenseCategory = expenseInputContainer.querySelector('#expense-category');
 
     if (validateInput(expenseName) === false) return;
     if (validateInput(expenseAmount) === false) return;
@@ -65,7 +71,7 @@ function createIncomeReport (name, amount, date, category)
     {
         this.id = Math.round(Math.random() * 1000000);
         this.name = name,
-        this.amount = amount,
+        this.amount = Number(amount),
         this.date = date,
         this.category = category
     }
@@ -80,7 +86,7 @@ function createExpenseReport (name, amount, date, category)
     {
         this.id = Math.round(Math.random() * 1000000);
         this.name = name,
-        this.amount = amount,
+        this.amount = Number(-amount),
         this.date = date,
         this.category = category
     }
@@ -97,20 +103,19 @@ function saveIncomeToLocalStorage(incomeObj)
 
     if (localStorage.getItem('Income Reports') === null)
     {
-        const incomeReportsGrouped = [];
-        incomeReportsGrouped.push(incomeObj);
+        const incomeReports = [];
+        incomeReports.push(incomeObj);
         
-        const jsonArray = JSON.stringify(incomeReportsGrouped);
+        const jsonArray = JSON.stringify(incomeReports);
         localStorage.setItem('Income Reports', jsonArray);
     }
     else 
     {
-        const jsonArray = localStorage.getItem('Income Reports');
-        const incomeReportsGrouped = JSON.parse(jsonArray);
+        const incomeReports = getIncomeReports();
 
-        incomeReportsGrouped.push(incomeObj);
+        incomeReports.push(incomeObj);
 
-        const jsonArrayUpdated = JSON.stringify(incomeReportsGrouped);
+        const jsonArrayUpdated = JSON.stringify(incomeReports);
         localStorage.setItem('Income Reports', jsonArrayUpdated);
     }
     
@@ -124,20 +129,18 @@ function saveExpenseToLocalStorage(expenseObj)
 
     if (localStorage.getItem('Expense Reports') === null)
     {
-        const expenseReportsGrouped = [];
-        expenseReportsGrouped.push(expenseObj);
+        const expenseReports = [];
+        expenseReports.push(expenseObj);
         
-        const jsonArray = JSON.stringify(expenseReportsGrouped);
+        const jsonArray = JSON.stringify(expenseReports);
         localStorage.setItem('Expense Reports', jsonArray);
     }
     else 
     {
-        const jsonArray = localStorage.getItem('Expense Reports');
-        const expenseReportsGrouped = JSON.parse(jsonArray);
+        const expenseReports = getExpenseReports();
+        expenseReports.push(expenseObj);
 
-        expenseReportsGrouped.push(expenseObj);
-
-        const jsonArrayUpdated = JSON.stringify(expenseReportsGrouped);
+        const jsonArrayUpdated = JSON.stringify(expenseReports);
         localStorage.setItem('Expense Reports', jsonArrayUpdated);
     }
     
@@ -145,28 +148,27 @@ function saveExpenseToLocalStorage(expenseObj)
 
 function reloadHtmlTable ()
 {
-    const tableBodyElement = document.querySelector('.financial-table-body');
-
+    clearTableBody();
+    let totalExpense = 0;
+    let totalIncome = 0;
+    let totalBalance = 0;
 
     if (localStorage.getItem('Expense Reports'))
     {
 
         (function () {
 
-            const jsonArray = localStorage.getItem('Expense Reports');
-            const expenseReportsGrouped = JSON.parse(jsonArray);
+            const expenseReports = getExpenseReports();
         
-            expenseReportsGrouped.forEach((expenseReport) => {
-                const tableRow = document.createElement('tr');
-        
-                tableRow.innerHTML = `
-                <td>${expenseReport.name}</td>
-                <td>${expenseReport.amount}</td>
-                <td>${expenseReport.date}</td>
-                <td class="hide-mobile">${expenseReport.category}</td>`;
-        
-                tableBodyElement.appendChild(tableRow);
+            expenseReports.forEach((expenseReport) => {
+
+                totalExpense += expenseReport.amount;
+               
+                addTableData(expenseReport);
+
             })
+
+            totalExpenseHeader.textContent = "Expense: " + totalExpense;
     
         })();
 
@@ -177,23 +179,110 @@ function reloadHtmlTable ()
 
         (function () {
 
-            const jsonArray = localStorage.getItem('Income Reports');
-            const incomeReportsGrouped = JSON.parse(jsonArray);
+            const incomeReports = getIncomeReports();
         
-            incomeReportsGrouped.forEach((incomeReport) => {
-                const tableRow = document.createElement('tr');
-        
-                tableRow.innerHTML = `
-                <td>${incomeReport.name}</td>
-                <td>${incomeReport.amount}</td>
-                <td>${incomeReport.date}</td>
-                <td class="hide-mobile">${incomeReport.category}</td>`;
-        
-                tableBodyElement.appendChild(tableRow);
+            incomeReports.forEach((incomeReport) => {
+                
+                totalIncome += incomeReport.amount;
+
+                addTableData(incomeReport);
+
             })
+
+            totalIncomeHeader.textContent = "Income: " + totalIncome;
     
         })();
 
     }
 
+    totalBalance = totalIncome + totalExpense;
+    totalBalanceHeader.textContent = "Balance: " + totalBalance;
+
+}
+
+const tableDate = document.querySelector('#table-date');
+
+tableDate.addEventListener('change', filterTableByDate);
+
+function filterTableByDate ()
+{
+    // If no date value show every report
+    // If date value is set show only reports according to the date
+    
+    if (tableDate.value === '')
+    {
+        reloadHtmlTable();
+    }
+    else
+    {
+        clearTableBody();
+
+        const incomeReports = getIncomeReports();
+        const expenseReports = getExpenseReports();
+
+        let totalIncome = 0;
+        let totalExpense = 0;
+        let totalBalance = 0;
+
+        incomeReports.forEach((incomeReport) => {
+            
+            if (incomeReport.date === tableDate.value)
+            {
+                addTableData(incomeReport);
+                totalIncome += incomeReport.amount;
+            }
+
+        })
+
+        expenseReports.forEach((expenseReport) => {
+            
+            if (expenseReport.date === tableDate.value)
+            {
+                addTableData(expenseReport);
+                totalExpense += expenseReport.amount;
+            }
+
+        })
+
+        totalBalance = totalExpense + totalIncome;
+
+        totalIncomeHeader.textContent = "Income: " + totalIncome;
+        totalExpenseHeader.textContent = "Expense: " + totalExpense;
+        totalBalanceHeader.textContent = "Balance: " + totalBalance;
+    }
+    
+}
+
+function addTableData (report)
+{
+    const tableRow = document.createElement('tr');
+        
+    tableRow.innerHTML = `
+    <td>${report.name}</td>
+    <td>${report.amount}$</td>
+    <td>${report.date}</td>
+    <td class="hide-mobile">${report.category}</td>`;
+
+    tableBodyElement.appendChild(tableRow);
+}
+
+function clearTableBody () 
+{
+    tableBodyElement.innerHTML = '';
+}
+
+function getExpenseReports ()
+{
+    const jsonArray = localStorage.getItem('Expense Reports');
+    const expenseReports = JSON.parse(jsonArray);
+
+    return expenseReports;
+}
+
+function getIncomeReports ()
+{
+    const jsonArray = localStorage.getItem('Income Reports');
+    const incomeReports = JSON.parse(jsonArray);
+
+    return incomeReports;
 }
